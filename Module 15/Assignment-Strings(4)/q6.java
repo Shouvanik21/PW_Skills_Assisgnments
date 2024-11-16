0005441a33045a929fcb10e9ed2b5813f97fe661
Shouvanik21 <143490804+Shouvanik21@users.noreply.github.com>
2024-11-16 23:59:05 +0530

    final commit

---
+++ Module 15/Assignment-Strings(4)/q6.java
@@ -0,0 +1,11 @@
+public class q6{
+    public static void main(String[] args) {
+     String s1 = "umbrella";
+     if(s1.contains("e")){
+        System.out.println("the string has the letter 'e' in it.");
+     }
+     else{
+        System.out.println("the string does not have the letter 'e' in it.");
+     }
+    }
+}
